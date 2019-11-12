e501122c2c8641360583e969d351b72f28b9c56d
Nick Corona <coronaisme@gmail.com>
2019-11-12 19:38:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ var logShout = (string) => console.log(string.toUpperCase());
 var logWhisper = (string) => console.log(string.toLowerCase());
 
 var sayHiToGrandma = (string) => 
-string = "HELLO!";
 if (string.toLowerCase() === string) {
   return `I can't hear you!`;
 }
